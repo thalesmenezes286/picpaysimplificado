34e4e2063ea54ac2e371f762660a366e38dbaf7e
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-11-25 11:06:23 -0300

    feat: entidades: User, Transaction - enum: UserType

---
+++ picpaysimplificado/src/main/java/com/dev/picpaysimplificado/entities/Transaction.java
@@ -0,0 +1,34 @@
+package com.dev.picpaysimplificado.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender; //remetente
+
+    @JoinColumn(name = "receiver_id")
+    private User receiver; //receptor
+
+    private LocalDate timestamp;
+}
---
+++ picpaysimplificado/src/main/java/com/dev/picpaysimplificado/entities/UserType.java
@@ -0,0 +1,6 @@
+package com.dev.picpaysimplificado.entities;
+
+public enum UserType {
+    COMMOM, //Usuário comum
+    MERCHANT; //Usuário logista
+}
